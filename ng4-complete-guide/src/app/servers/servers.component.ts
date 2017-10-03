67177816e2a1f5ec0be6ccba241eea14cd83066f
Ricardo <4rfvj7ujm>
2017-10-03 09:09:41 +0100

    The Basics - creating components

--- ng4-complete-guide/src/app/servers/servers.component.ts
+++ ng4-complete-guide/src/app/servers/servers.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: 'app-servers',
+  selector: 'app-servers',
   //selector: '[app-servers]',
-  selector: '.app-servers',
+  //selector: '.app-servers',
 
-  //templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  //template: `
+  //  <app-server></app-server>
+  //  <app-server></app-server>`,
 
   styleUrls: ['./servers.component.css']
-  //styles:[`h3{ color: dodgerblue; }`]
+  //styles:[`h3{ color: red; }`]
 })
 export class ServersComponent implements OnInit {
 
