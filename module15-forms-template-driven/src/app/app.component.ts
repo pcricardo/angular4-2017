bd1673a6f79d3f1ee7f72db69cea40fdd79d9c8b
Ricardo <4rfvj7ujm>
2018-02-19 20:24:00 +0000

    Forms

--- module15-forms-template-driven/src/app/app.component.ts
+++ module15-forms-template-driven/src/app/app.component.ts
@@ -8,15 +8,46 @@ import { NgForm  } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+	username: '',
+	email: '',
+	secretQuestion: '',
+	answer: '',
+	gender: ''
+  };
+  submitted = false;
   
   suggestUserName() {
     const suggestedName = 'Superuser';
+//	this.signupForm.setValue({
+//		userData: {
+//			username: suggestedName,
+//			email: ''
+//		},
+//		secret: 'pet',
+//		questionAnswer: '',
+//		gender: 'male'
+//	});
+	this.signupForm.form.patchValue({
+		userData: {
+			username: suggestedName
+		},
+	});
   }
   
   //onSubmit(form: NgForm) {
 	//console.log(form);
   //}
   onSubmit() {
-	console.log(this.signupForm);
+	//console.log(this.signupForm);
+	this.submitted = true;
+	this.user.username = this.signupForm.value.userData.username;
+	this.user.email = this.signupForm.value.userData.email;
+	this.user.secretQuestion = this.signupForm.value.secret;
+	this.user.answer = this.signupForm.value.questionAnswer;
+	this.user.gender = this.signupForm.value.gender;	
   }
 }
